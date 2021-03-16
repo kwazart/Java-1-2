fb857f2df92af56214b1519f62d190c6216bc0d3
Mihail <ptichkinm@mail.ru>
2021-03-16 22:25:44 +0300

    ДЗ-2.1

--- src/homework02/Main.java
+++ src/homework02/Main.java
@@ -4,39 +4,38 @@ import homework02.exceptions.MyArrayDataException;
 import homework02.exceptions.MyArraySizeException;
 
 public class Main {
+
 	public static void main(String[] args) {
 		String[][] array = {
 				{"1", "2", "3", "4"},
 				{"5", "6", "7", "8"},
 				{"9", "10", "11", "12"},
-				{"13", "14", "16"}
+				{"13", "14", "15", "16"}
 		};
 
-		method1(array);
+//	3. В методе main() вызвать полученный метод, обработать возможные исключения MySizeArrayException и MyArrayDataException, и вывести результат расчета.
+
+		try {
+			method1(array);
+		} catch (MyArrayDataException e) {
+			e.printStackTrace();
+		} catch (MyArraySizeException e) {
+			e.printStackTrace();
+		}
 	}
 
 //	1. Напишите метод, на вход которого подаётся двумерный строковый массив размером 4х4, при подаче массива другого размера необходимо бросить исключение MyArraySizeException.
 
-	public static void method1 (String[][] arr) {
+	public static void method1 (String[][] arr) throws MyArrayDataException, MyArraySizeException {
 
 		if (arr.length == 4) {
 			for (String[] str : arr) {
 				if (str.length != 4) {
-					try {
-						throw new MyArraySizeException();
-					} catch (MyArraySizeException e) {
-						e.printStackTrace();
-						return;
-					}
+					throw new MyArraySizeException();
 				}
 			}
 		} else {
-			try {
-				throw new MyArraySizeException();
-			} catch (MyArraySizeException e) {
-				e.printStackTrace();
-				return;
-			}
+			throw new MyArraySizeException();
 		}
 
 //	2. Далее метод должен пройтись по всем элементам массива, преобразовать в int, и просуммировать. Если в каком-то элементе массива преобразование не удалось (например, в ячейке лежит символ или текст вместо числа), должно быть брошено исключение MyArrayDataException, с детализацией в какой именно ячейке лежат неверные данные.
@@ -46,11 +45,7 @@ public class Main {
 				try {
 					sum += Integer.parseInt(arr[i][j]);
 				} catch (Exception e) {
-					try {
-						throw new MyArrayDataException("Неверные данные в ячейке [" + i + "][" + j +"]");
-					} catch (MyArrayDataException myArrayDataException) {
-						myArrayDataException.printStackTrace();
-					}
+					throw new MyArrayDataException("Неверные данные в ячейке [" + i + "][" + j +"]");
 				}
 			}
 		}
@@ -59,5 +54,4 @@ public class Main {
 
 	}
 
-//	3. В методе main() вызвать полученный метод, обработать возможные исключения MySizeArrayException и MyArrayDataException, и вывести результат расчета.
 }
